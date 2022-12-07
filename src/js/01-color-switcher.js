9beed5297cdb1654aa2e9d0eba6ce187da3bfbef
Ruslan Karpov <rkarpov88@gmail.com>
2022-12-07 09:04:37 +0200

    task 01 finished

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const btnClickStart = document.querySelector('button[data-start]');
+const btnClickStop = document.querySelector('button[data-stop]');
+let timerId = null;
+btnClickStop.disabled = true;
 
+const onClickColorChange = () => {
+    btnClickStart.disabled = true;
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    btnClickStop.disabled = false;
+}
+
+
+btnClickStart.addEventListener('click', onClickColorChange);
+btnClickStop.addEventListener('click', () => {
+    clearInterval(timerId);
+    btnClickStop.disabled = true;
+    btnClickStart.disabled = false;
+});
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
